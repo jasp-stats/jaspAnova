c265fee8bf61c76fa2bc3d9e653ecdfee3cef276
vandenman <donvdbergh@hotmail.com>
2021-07-30 13:59:49 +0200

    another attemp at fixing 1181

--- R/commonAnovaBayesian.R
+++ R/commonAnovaBayesian.R
@@ -1740,36 +1740,36 @@ BANOVAcomputMatchedInclusion <- function(effectNames, effects.matrix, interactio
   if (!is.null(state)) { # can we reuse some posteriors?
     reuseable <- model[["reuseable"]]
 
-    if (model[["analysisType"]] == "RM-ANOVA") {
-      # it's possible that a user just renames a level of a repeated measures factor
-      # in that case everything can be reused, but we have to rename some parameters.
+    # it's possible that a user just renames a level of a repeated measures factor
+    # in that case everything can be reused, but we have to rename some parameters.
+    # the same may happen when a variable becomes a nuisance parameter, because then
+    # BayesFactor changes it's order in the names.
 
-      oldLevelInfo <- state[["levelInfo"]]$levelNames
-      newLevelInfo <- levelInfo$levelNames
+    oldLevelInfo <- state[["levelInfo"]]$levelNames
+    newLevelInfo <- levelInfo$levelNames
 
-      sortTerms <- function(x) {
-        # split b:a to c("b", "a"), sort it, and then paste it back
-        # otherwise posteriors samples between different runs are not correctly retrieved from the state.
-        sapply(strsplit(names(oldLevelInfo), ":", fixed = TRUE), function(x) paste(sort(x), collapse = ":"))
-      }
+    sortTerms <- function(x) {
+      # split b:a to c("b", "a"), sort it, and then paste it back
+      # otherwise posteriors samples between different runs are not correctly retrieved from the state.
+      sapply(strsplit(names(oldLevelInfo), ":", fixed = TRUE), function(x) paste(sort(x), collapse = ":"))
+    }
 
-      tmp_old <- sortTerms(names(oldLevelInfo))
-      tmp_new <- sortTerms(names(newLevelInfo))
-      oldNames <- names(oldLevelInfo)
-      newNames <- names(newLevelInfo)
-      names(oldNames) <- tmp_old
-      names(newNames) <- tmp_new
+    tmp_old <- sortTerms(names(oldLevelInfo))
+    tmp_new <- sortTerms(names(newLevelInfo))
+    oldNames <- names(oldLevelInfo)
+    newNames <- names(newLevelInfo)
+    names(oldNames) <- tmp_old
+    names(newNames) <- tmp_new
 
-      if (!identical(newLevelInfo, oldLevelInfo)) {
-        for (nm in intersect(names(oldNames), names(newNames))) {
+    if (!identical(newLevelInfo, oldLevelInfo)) {
+      for (nm in intersect(names(oldNames), names(newNames))) {
 
-          nm_old <- oldNames[nm]
-          nm_new <- newNames[nm]
+        nm_old <- oldNames[nm]
+        nm_new <- newNames[nm]
 
-          idx <- !(oldLevelInfo[[nm_old]] %in% newLevelInfo[[nm_new]])
-          renameFrom <- c(renameFrom, oldLevelInfo[[nm_old]][idx])
-          renameTo   <- c(renameTo,   newLevelInfo[[nm_new]][idx])
-        }
+        idx <- !(oldLevelInfo[[nm_old]] %in% newLevelInfo[[nm_new]])
+        renameFrom <- c(renameFrom, oldLevelInfo[[nm_old]][idx])
+        renameTo   <- c(renameTo,   newLevelInfo[[nm_new]][idx])
       }
     }
   } else {

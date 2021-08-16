d17f25280b6a2bf760c1159b9396e2efebb401dd
vandenman <donvdbergh@hotmail.com>
2021-08-16 11:21:18 +0200

    remove .unv from posthoc statement

--- R/ancova.R
+++ R/ancova.R
@@ -925,10 +925,8 @@ Ancova <- function(jaspResults, dataset = NULL, options) {
     colnames(allPvalues) <- postHocCorrections
     resultPostHoc <- cbind(resultPostHoc[[1]], allPvalues)
 
-    resultPostHoc[["contrast_A"]] <- lapply(allContrasts, function(x) paste(.unv(strsplit(x[[1]], "[ ,]")[[1]]), 
-                                                                           collapse = ", "))
-    resultPostHoc[["contrast_B"]] <- lapply(allContrasts, function(x) paste(.unv(strsplit(x[[2]], "[ ,]")[[1]]), 
-                                                                           collapse = ", "))
+    resultPostHoc[["contrast_A"]] <- lapply(allContrasts, function(x) paste(strsplit(x[[1]], "[ ,]")[[1]], collapse = ", "))
+    resultPostHoc[["contrast_B"]] <- lapply(allContrasts, function(x) paste(strsplit(x[[2]], "[ ,]")[[1]], collapse = ", "))
 
     if (options$postHocTestsBootstrapping) {
 

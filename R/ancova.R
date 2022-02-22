449a88f1dd95d7782045afdfc5d57a82f416993d
Kucharssim <kucharssim@gmail.com>
2022-02-22 10:36:12 +0100

    improve bootstrap progress

--- R/ancova.R
+++ R/ancova.R
@@ -836,6 +836,7 @@ Ancova <- function(jaspResults, dataset = NULL, options) {
 }
 
 .anovaOrdinalRestrictions <- function(anovaContainer, dataset, options, ready) {
+  t1 <- Sys.time()
   if (!ready) return()
 
   options[["restrictedModels"]] <- .aorPruneEmptyModels(options[["restrictedModels"]])
@@ -846,8 +847,10 @@ Ancova <- function(jaspResults, dataset = NULL, options) {
 
   if(length(options[["restrictedModels"]]) == 0) return()
 
-  .aorModelComparison      (container, dataset, options)
-  .aorSingleModelsInference(container, dataset, options)
+  models <- .aorGetFittedModels(container, dataset, options)
+
+  .aorModelComparison      (container, dataset, options, models)
+  .aorSingleModelsInference(container, dataset, options, models)
 
   return()
 }

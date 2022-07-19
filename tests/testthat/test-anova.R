86ac8f031b6d6a95f3174ca32396a18748678f7a
Simon Kucharsky <kucharssim@gmail.com>
2022-07-19 15:38:38 +0200

    fix unit tests (#185)

--- tests/testthat/test-anova.R
+++ tests/testthat/test-anova.R
@@ -150,7 +150,7 @@ test_that("Post Hoc table results match", {
   table <- results$results$anovaContainer$collection$anovaContainer_postHocContainer$collection$anovaContainer_postHocContainer_postHocStandardContainer$collection$anovaContainer_postHocContainer_postHocStandardContainer_contBinom$data
   jaspTools::expect_equal_tables(table,
                                  list("TRUE", 0.214904085649005, 0.448976320466698, 0.15401876311258,
-                                      -0.24864690950018, 0.55668443572534, "contBinom0", "contBinom1", 0.163364220743842,
+                                      -0.24864690950018, 0.55668443572534, 0, 1, 0.163364220743842,
                                       0.448976320466698, -0.263105943067512, 0.448976320466698, 0.448976320466698,
                                       0.760172707980336, 0.448976320466698, 0.589834384555196)
     )
@@ -452,11 +452,11 @@ test_that("Field - Chapter 5 results match", {
   # standard post hoc (tukey)
   table <- results[["results"]]$anovaContainer$collection$anovaContainer_postHocContainer$collection$anovaContainer_postHocContainer_postHocStandardContainer$collection$anovaContainer_postHocContainer_postHocStandardContainer_Dose$data
   jaspTools::expect_equal_tables(table,
-                                 list("TRUE", 0.886942313043338, "Dose1", "Dose2", -1, -3.36624115850687, -1.12746904200424,
-                                      0.516276123508473, 1.36624115850687, "FALSE", 0.886942313043338,
-                                      "Dose1", "Dose3", -2.8, -5.16624115850687, -3.15691331761188, 0.0209243994922408,
-                                      -0.433758841493134, "FALSE", 0.886942313043338, "Dose2", "Dose3", -1.8,
-                                      -4.16624115850687, -2.02944427560764, 0.147457622995377, 0.566241158506866
+                                 list("TRUE", 0.886942313043338, 1, 2, -1, -3.36624115850686, -1.12746904200424,
+                                      0.516276123508473, 1.36624115850686, "FALSE", 0.886942313043338,
+                                      1, 3, -2.8, -5.16624115850687, -3.15691331761188, 0.0209243994922408,
+                                      -0.433758841493136, "FALSE", 0.886942313043338, 2, 3, -1.8,
+                                      -4.16624115850686, -2.02944427560764, 0.147457622995377, 0.566241158506864
                                  ))
 
   # games-howell post hoc
@@ -527,15 +527,15 @@ test_that("Field - Chapter 7 results match", {
                            0.147339575330745, "FALSE"))
 
   # removed both post hoc table and contrast table because bootstrap results are now in same table
-  table <- results[["results"]]$anovaContainer$collection$anovaContainer_postHocContainer$collection[[1]]$collection$anovaContainer_postHocContainer_postHocStandardContainer_Alcohol$data
+  table <- results[["results"]]$anovaContainer$collection$anovaContainer_postHocContainer$collection$anovaContainer_postHocContainer_postHocStandardContainer$collection$anovaContainer_postHocContainer_postHocStandardContainer_Alcohol$data
   jaspTools::expect_equal_tables(table,
                                  list("TRUE", 0.392000159227314, -0.00376914898826786, 0.230950085511107,
-                                      "Alcohol0", "Alcohol1", -0.769579725829724, -1.48733254329573, -1.8129965586672,
+                                      0, 1, -0.769579725829724, -1.48733254329573, -1.8129965586672,
                                       0.177726007657148, 0.0548973391001529, "FALSE", 0.435378856593833,
-                                      0.0142934574412505, 0.00359956767679779, "Alcohol0", "Alcohol2", -1.43536324786325,
+                                      0.0142934574412505, 0.00359956767679779, 0, 2, -1.43536324786325,
                                       -2.31465226049576, -3.47491007077881, 0.00337043014651417, -0.602581004497449,
                                       "FALSE", 0.407611902363181, 0.0180626064295184, 0.311931949521,
-                                      "Alcohol1", "Alcohol2", -0.690674603174603, -1.43846891737073, -1.66191351211161,
+                                      1, 2, -0.690674603174603, -1.43846891737073, -1.66191351211161,
                                       0.231712504393661, 0.214248742598683))
 
 

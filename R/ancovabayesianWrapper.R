a391ce01cfa9c174df1fb02dae074e307a815bbd
boutinb <b.boutin@uva.nl>
2022-12-12 16:16:55 +0100

    Add Wrappers
    Clear up also the QML Import statements

---
+++ R/ancovabayesianWrapper.R
@@ -0,0 +1,108 @@
+#
+# Copyright (C) 2013-2022 University of Amsterdam
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# This is a generated file. Don't change it
+
+AncovaBayesian <- function(
+          data = NULL,
+          version = "0.17",
+          formula = NULL,
+          isNuisance = NULL,
+          bayesFactorOrder = "bestModelTop",
+          bayesFactorType = "BF10",
+          bernoulliParameter = 0.5,
+          betaBinomialParameterA = 1,
+          betaBinomialParameterB = 1,
+          castilloParameterU = 1,
+          cauchyPriorScaleCovariates = 0.354,
+          cauchyPriorScaleFixedEffects = 0.5,
+          cauchyPriorScaleRandomEffects = 1,
+          covariates = list(),
+          credibleInterval = 0.95,
+          criTable = FALSE,
+          customPriorSpecification = list(),
+          dependent = "",
+          descriptivePlotCi = FALSE,
+          descriptivePlotCiLevel = 0.95,
+          descriptivePlotHorizontalAxis = "",
+          descriptivePlotSeparateLines = "",
+          descriptivePlotSeparatePlot = "",
+          descriptives = FALSE,
+          effects = FALSE,
+          effectsType = "allModels",
+          enforcePrincipleOfMarginalityFixedEffects = TRUE,
+          enforcePrincipleOfMarginalityRandomSlopes = FALSE,
+          fixedFactors = list(),
+          groupPosterior = "grouped",
+          hideNuisanceParameters = TRUE,
+          integrationMethod = "automatic",
+          legacyResults = FALSE,
+          modelAveragedPosteriorPlot = FALSE,
+          modelPrior = "uniform",
+          modelTerms = list(),
+          modelsShown = "limited",
+          numModelsShown = 10,
+          plotHeight = 320,
+          plotWidth = 480,
+          postHocNullControl = TRUE,
+          postHocTerms = list(),
+          posteriorEstimates = FALSE,
+          priorSpecificationMode = "acrossParameters",
+          qqPlot = FALSE,
+          rainCloudHorizontalAxis = "",
+          rainCloudHorizontalDisplay = FALSE,
+          rainCloudSeparatePlots = "",
+          rainCloudYAxisLabel = "",
+          randomFactors = list(),
+          rsqPlot = FALSE,
+          samplesMCMC = 1000,
+          samplesNumericAccuracy = 10000,
+          samplingMethodMCMC = "auto",
+          samplingMethodNumericAccuracy = "auto",
+          seed = 1,
+          setSeed = FALSE,
+          singleModelCriTable = FALSE,
+          singleModelEstimates = FALSE,
+          singleModelGroupPosterior = "grouped",
+          singleModelPosteriorPlot = FALSE,
+          singleModelQqPlot = FALSE,
+          singleModelRsqPlot = FALSE,
+          singleModelTerms = list(),
+          wilsonParameterLambda = 1) {
+
+   defaultArgCalls <- formals(jaspAnova::AncovaBayesian)
+   defaultArgs <- lapply(defaultArgCalls, eval)
+   options <- as.list(match.call())[-1L]
+   options <- lapply(options, eval)
+   defaults <- setdiff(names(defaultArgs), names(options))
+   options[defaults] <- defaultArgs[defaults]
+   options[["data"]] <- NULL
+   options[["version"]] <- NULL
+
+   if (!is.null(formula)) {
+      if (!inherits(formula, "formula")) {
+         formula <- as.formula(formula)
+      }
+      options$formula <- jaspBase::jaspFormula(formula, data)
+   }
+
+   optionsWithFormula <- c("isNuisance", "covariates", "customPriorSpecification", "dependent", "descriptivePlotHorizontalAxis", "descriptivePlotSeparateLines", "descriptivePlotSeparatePlot", "fixedFactors", "modelTerms", "postHocTerms", "rainCloudHorizontalAxis", "rainCloudSeparatePlots", "randomFactors", "singleModelTerms")
+   for (name in optionsWithFormula) {
+      if ((name %in% optionsWithFormula) && inherits(options[[name]], "formula")) options[[name]] = jaspBase::jaspFormula(options[[name]], data)   }
+
+   return(jaspBase::runWrappedAnalysis("jaspAnova::AncovaBayesian", data, options, version))
+}
--- inst/qml/common/bayesian/AdditionalOptions.qml
+++ inst/qml/common/bayesian/AdditionalOptions.qml
@@ -16,11 +16,10 @@
 // <http://www.gnu.org/licenses/>.
 //
 
-import QtQuick			2.12
-import QtQuick.Layouts	1.3
-import JASP.Controls	1.0
-import JASP.Widgets		1.0
-import JASP				1.0
+import QtQuick
+import QtQuick.Layouts
+import JASP
+import JASP.Controls
 import "../." as Common
 
 Section

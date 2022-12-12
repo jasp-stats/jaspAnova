98ede87d4fcb9512b0d45b73f0fc42aa81e7f4a9
Kucharssim <kucharssim@gmail.com>
2022-12-12 17:07:32 +0100

    fix rm-anova issue with sidak post-hox

--- R/anovarepeatedmeasures.R
+++ R/anovarepeatedmeasures.R
@@ -1190,7 +1190,8 @@ AnovaRepeatedMeasures <- function(jaspResults, dataset = NULL, options) {
   if (options$postHocCorrectionHolm)
     postHocTable$addColumnInfo(name="holm", title=gettext("p<sub>holm</sub>"), type="pvalue")
 
-  if (options$postHocCorrectionSidak)
+  # Sidak option does not exist in RM-ANOVA
+  if (isTRUE(options$postHocCorrectionSidak))
     postHocTable$addColumnInfo(name="sidak", title=gettext("p<sub>sidak</sub>"), type="pvalue")
 
 
@@ -1566,7 +1567,7 @@ AnovaRepeatedMeasures <- function(jaspResults, dataset = NULL, options) {
       friedmanTable$setError(gettextf("Not an unreplicated complete block design."))
       return()
     }
-    
+
     t <- nlevels(groups)
     b <- nlevels(blocks)
     r <- unique(table(groups))

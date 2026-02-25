c2d85459e472a2d3a9b89d4c4bf3b3da878690ed
Johnny <j.b.vandoorn@uva.nl>
2026-02-25 14:46:27 +0100

    implement t logic helpers

--- tests/testthat/helper-anova.R
+++ tests/testthat/helper-anova.R
@@ -9,11 +9,25 @@ initClassicalAnovaOptions <- function(analysis = c("Anova", "Ancova", "AnovaRepe
 }
 
 classicalAnovaCommonOptions <- function() {
-  path <- testthat::test_path("..", "..", "inst", "qml", "common", "classical")
+  path <- if (nzchar(Sys.getenv("R_COVR"))) {
+    # We are running inside covr (installed package structure)
+    system.file("qml", "common", "classical", package = "jaspANOVA")
+  } else {
+    testthat::test_path(file.path("..", "..", "inst", "qml", "common", "classical"))
+  }
+  
   files <- list.files(path, full.names = TRUE)
+  
+  commonPath <- if (nzchar(Sys.getenv("R_COVR"))) {
+    # We are running inside covr (installed package structure)
+    system.file("qml", "common", package = "jaspANOVA")
+  } else {
+    testthat::test_path(file.path("..", "..", "inst", "qml", "common"))
+  }
+  
   files <- c(
     files,
-    testthat::test_path("..", "..", "inst", "qml", "common", "RainCloudPlots.qml")
+    file.path(commonPath, "RainCloudPlots.qml")
   )
   options <- lapply(files, jaspTools:::readQML) |>
     lapply(function(x) {x$plotWidth <- NULL; x$plotHeight <- NULL; return(x)}) |>
--- tests/testthat/helper-banova.R
+++ tests/testthat/helper-banova.R
@@ -16,13 +16,20 @@ initOpts <- function(analysisName) {
 
 addCommonQMLoptions <- function(options) {
   # jaspTools doesn't recognize common QML elements so this function adds the defaults manually
-  root <- testthat::test_path(file.path("..", "..", "inst", "qml", "common", "bayesian"))
+  root <- if (nzchar(Sys.getenv("R_COVR"))) {
+    # We are running inside covr (installed package structure)
+    system.file("qml", "common", "bayesian", package = "jaspANOVA")
+  } else {
+    testthat::test_path(file.path("..", "..", "inst", "qml", "common", "bayesian"))
+  }
   c(
     options,
     jaspTools:::readQML(file.path(root, "DefaultOptions.qml")),
     jaspTools:::readQML(file.path(root, "ModelTerms.qml")),
     jaspTools:::readQML(file.path(root, "SingleModelInference.qml")),
     jaspTools:::readQML(file.path(root, "DescriptivesPlots.qml")),
-    jaspTools:::readQML(file.path(root, "AdditionalOptions.qml"))
+    jaspTools:::readQML(file.path(root, "AdditionalOptions.qml")),
+    jaspTools:::readQML(file.path(root, "BayesFactorOrder.qml")),
+    jaspTools:::readQML(file.path(root, "PostHocTests.qml"))
   )
 }
